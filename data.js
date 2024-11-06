42c2c9def3215edfddaac9068aaaad1c85075dbf
Hindi-Design-Studios <hindidesignstudio.net@gmail.com>
2024-11-06 22:01:16 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,101 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-panorama_bedroom-animation-1000x1500_2024-10-09-17-58-44",
+      "name": "Panorama_Bedroom Animation 1000x1500_2024-10-09-17-58-44",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 1.6102134966170638,
+        "pitch": 0.001219244474208736,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.1743902415522829,
+          "pitch": -0.030917924697373422,
+          "rotation": 0,
+          "target": "1-panorama_girls-bedroom_2024-10-11-17-03-37"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.16860183694927677,
+          "pitch": -0.17532382749987363,
+          "title": "Click to Switch",
+          "text": "If you click this button ,you can see the kids bedroom 360 vr."
+        }
+      ]
+    },
+    {
+      "id": "1-panorama_girls-bedroom_2024-10-11-17-03-37",
+      "name": "Panorama_Girls Bedroom_2024-10-11-17-03-37",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 2.180704039720087,
+        "pitch": -0.04951799586651617,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.723605932141794,
+          "pitch": -0.08207260998354116,
+          "rotation": 0,
+          "target": "0-panorama_bedroom-animation-1000x1500_2024-10-09-17-58-44"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.7421451850366303,
+          "pitch": -0.21059612606786793,
+          "title": "Click here",
+          "text": "to view master bedroom"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
